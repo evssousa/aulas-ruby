aab0dceb1bdb983715f84e5a6e1bece095d344a9
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2025-10-19 14:24:23 +0000

    update: explicações adicionadas

--- modulo2-arquivos-modulos-gems/07_projeto1_busca_palavras_em_arquivo/main.rb
+++ modulo2-arquivos-modulos-gems/07_projeto1_busca_palavras_em_arquivo/main.rb
@@ -9,23 +9,22 @@
 puts "= " * 10 # 1-Forma 1
 # =========================================================
 def procurar(chave)
-    # procurando tudo que contém .txt
-    Dir.glob("data/*.txt") do |arquivo|
-        arq = File.open(arquivo)
-        arq.each_with_index do |linha, index|
-            if linha.include?(chave)
-                return arquivo, index
+    Dir.glob("data/*.txt") do |arquivo|         # procurando tudo que contém .txt na pasta data
+        arq = File.open(arquivo)                # abre o arquivo e atribui a palavra lida na variável
+        arq.each_with_index do |linha, index|   # pega a linha e o index da palavra procurada
+            if linha.include?(chave)            # verificação se o que foi procurado existe
+                return arquivo, index           # retorna qual arquivo e o index da palavra
             end
         end
-        arq.close
+        arq.close                               # fecha o arquivo
     end
 end
 
-puts "= = = Busca palavras em arquivos = = ="
-print "Digite uma palavra a ser procurada: "
-chave = gets.chomp
+puts "= = = Busca palavras em arquivos = = ="   
+print "Digite uma palavra a ser procurada: "    # palavra a ser digitada
+chave = gets.chomp                              # palavra digitada atribuida a variável chave
 
-nomeArquivo, numeroLinha = procurar(chave)
+nomeArquivo, numeroLinha = procurar(chave)      # a chamada vai retornar as duas informações: arquivo e index (linha 16)
 puts "Arquivo: #{nomeArquivo} | Linha: #{numeroLinha + 1}"
 
 # Resultado:
