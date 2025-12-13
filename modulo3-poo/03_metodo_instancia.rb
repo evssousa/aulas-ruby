e57a569c775a69a801871359e12351673e7398df
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2025-12-13 20:00:06 +0000

    aula finalizada

--- modulo3-poo/03_metodo_instancia.rb
+++ modulo3-poo/03_metodo_instancia.rb
@@ -24,4 +24,15 @@ class Game
     def informacao
         puts "O jogo é o gênero #{@genero} e as versões: #{@versao}"
     end
+
+    def dados_tecnicos
+        puts "## Dados do Jogo ##"
+        puts "Nome do jogo: #{@nome}"
+        puts "Gênero do jogo: #{@genero}"
+        puts "Preço do jogo: R$#{@preco}"
+    end
 end
+
+game = Game.new
+puts game.informacao
+puts game.dados_tecnicos
