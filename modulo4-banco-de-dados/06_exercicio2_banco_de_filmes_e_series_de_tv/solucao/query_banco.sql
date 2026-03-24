1752fcc482151aeffbc107dd961c52adacec6fa4
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2026-03-24 12:23:35 +0000

    refactory: inserção de dados em query_banco.sql

--- modulo4-banco-de-dados/06_exercicio2_banco_de_filmes_e_series_de_tv/solucao/query_banco.sql
+++ modulo4-banco-de-dados/06_exercicio2_banco_de_filmes_e_series_de_tv/solucao/query_banco.sql
@@ -14,7 +14,7 @@ INSERT INTO Filmes (titulo, diretor, ano, genero, duracao, avaliacao, bilheteria
 VALUES
     ('Mad Max: Fury Road', 'George Miller', 2015, 'Ação', 120, 8.1, '375.2000.000,00', '150.000.000,00'),
     ('Star Wars', 'George Lucas', 1977, 'Sci-Fi', 121, 8.6, '775.398.007,00', '11.000.000,00'),
-    ('Super Mario, Bros', 'Aaron Horvath, Michael Jelenic', 2023, 'Animação', 92, 7.3, '1.3000.000.000,00', '1000.000.000,00')
+    ('Super Mario, Bros', 'Aaron Horvath, Michael Jelenic', 2023, 'Animação', 92, 7.3, '1.3000.000.000,00', '100.000.000,00')
 
 CREATE TABLE IF NOT EXISTS Series_TV (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
