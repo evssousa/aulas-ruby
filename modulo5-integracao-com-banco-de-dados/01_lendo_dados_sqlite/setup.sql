6dbb29e808ea9a23a36845b741952846b4e8513a
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2026-04-17 20:28:53 +0000

    feat: adiciona script Ruby para inserção de dados no SQLite

--- modulo5-integracao-com-banco-de-dados/01_lendo_dados_sqlite/setup.sql
+++ modulo5-integracao-com-banco-de-dados/01_lendo_dados_sqlite/setup.sql
@@ -8,9 +8,10 @@ CREATE TABLE IF NOT EXISTS jogo (
   score INTEGER
 );
 
-INSERT INTO jogo (name, year, score) VALUES
-('Mario', 1985, 95),
-('Zelda', 1986, 98),
-('Sonic', 1991, 90);
+INSERT INTO jogo (name, year, score) 
+VALUES
+  ('Mario', 1985, 95),
+  ('Zelda', 1986, 98),
+  ('Sonic', 1991, 90);
 
 -- Execute no terminal: >sqlite3 fliperama.db < setup.sql
